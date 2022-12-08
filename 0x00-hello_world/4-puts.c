00bb9324edf5df5bc5d37020bcb048265acb6737
uzo-john <johnpauluzowuru2018@gmail.com>
2022-12-08 07:54:21 -0800

    done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void) {
+
+
+    put("\"Programming is like building a multilingual puzzle");
+    
+    return (0);
+
+}
+
