4256020fac81aa5fa18531142432a62e0811ccc6
uzo-john <johnpauluzowuru2018@gmail.com>
2022-12-09 15:15:47 -0800

    mvp

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - return single interger from 0 to 10
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int num = 0;
+	
+	while (num < 10)
+	{
+		printf("%d", num);
+		
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
