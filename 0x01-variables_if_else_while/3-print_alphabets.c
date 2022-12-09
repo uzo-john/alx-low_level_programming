e869298058d17ca0c0e12b10dee8cb100ef66b6b
uzo-john <johnpauluzowuru2018@gmail.com>
2022-12-09 13:58:29 -0800

    mvp

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,11 +6,9 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int ch = 'a';
-			
 	int CH = 'A';
 
 	while (ch <= 'z')
@@ -23,7 +21,6 @@ int main(void)
 		putchar(CH);
 		CH++;
 	}
-
 	putchar('\n');
-        return (0);
+	return (0);
 }
