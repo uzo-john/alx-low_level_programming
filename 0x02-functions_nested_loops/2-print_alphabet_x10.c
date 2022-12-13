e4afb435323aa91dfaa7002cb47dd3d1c815e86c
uzo-john <johnpauluzowuru2018@gmail.com>
2022-12-13 08:58:45 -0800

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "mine.h"
+
+/**
+ * print_alphabet_x10 - print a - z ten times
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int g;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (g = 'a'; g <= 'z'; g++)
+		{
+			_putchar(g);
+		}
+		_putchar('\n');
+	}
+}
