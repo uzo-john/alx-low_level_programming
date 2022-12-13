8739e4e5c8dbfc82738f6ec6b8d0bd2ea9f0682c
uzo-john <johnpauluzowuru2018@gmail.com>
2022-12-13 13:37:23 -0800

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -37,8 +37,8 @@ void times_table(void)
 																			_putchar((c % 10) + '0');
 																			if (b < 9)
 																			{
-																				_putchsr(',');
-																				_putchsr(' ');
+																				_putchar(',');
+																				_putchar(' ');
 																			}
 																		}
 		}
