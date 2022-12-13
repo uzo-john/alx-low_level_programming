6faa7b4966b8288be51f566fee81ad73e4cca004
uzo-john <johnpauluzowuru2018@gmail.com>
2022-12-13 14:48:26 -0800

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 	{
 		for (; n > 98; n--)
 		{
-			printf("%d, ", 98);
+			printf("%d, ", n);
 		}
 	}
 	else if (n < 98)
@@ -22,5 +22,5 @@ void print_to_98(int n)
 			printf("%d, ", n);
 		}
 	}
-	printf("%d\n", 98);
+	printf("%d\n", n);
 }
