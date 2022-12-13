bf007e90dda96ce5aa0e537a9858925ef8dc524d
uzo-john <johnpauluzowuru2018@gmail.com>
2022-12-13 07:44:34 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#inclde "main.h"
+#include "main.h"
 
 /**
  * main - Entry point
@@ -15,6 +15,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
