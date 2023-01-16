8db0a9b70a7c51661c7899debbead0b4e2603968
uzo-john <johnpauluzowuru2018@gmail.com>
2023-01-16 08:21:57 -0800

    task

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -4,17 +4,14 @@
 /**
  * free_dog - free memory of dog
  * @d: pointer to dog
+ * Return: no return.
  */
-
 void free_dog(dog_t *d)
 {
 	if (d)
 	{
-		if (d->name)
-			free(d->name);
-		if (d->owner)
-			free(d->owner);
-
+		free(d->name);
+		free(d->owner);
 		free(d);
 	}
 }
