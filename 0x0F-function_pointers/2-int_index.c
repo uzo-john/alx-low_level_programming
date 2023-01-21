ff652d6bc76385f9db21c99d42997b50c0b88167
uzo-john <johnpauluzowuru2018@gmail.com>
2023-01-20 18:46:54 -0800

    task

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,11 +17,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (array && cmp)
 	{
+		if (size <= 0)
+			return (-1);
+
 		for (i = 0; i < size; i++)
-		{
-			for (i = 0; i < size; i++)
+			if (cmp(array[i]))
 				return (i);
-		}
 	}
+
 	return (-1);
 }
