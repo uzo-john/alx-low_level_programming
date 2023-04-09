551d3284c36b250c456fd1bb9add97f335cacf52
uzo-john <johnpauluzowuru2018@gmail.com>
2023-04-09 14:24:13 -0700

    task

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -10,7 +10,6 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int hash_val = hash_djb2(key);
 
 	if (key == NULL || size == 0)
 		return (0);
